9c6a4addddc24fe3b61b0298e70939e0dc30aeb8
M Urwah Shahid <urwahpatel@gmail.com>
2022-08-28 16:00:57 +0500

    Done with this

--- src/index.js
+++ src/index.js
@@ -1,22 +1,41 @@
 // Toggle button Function for navbar
 let btn = document.getElementById("menu-toggle");
 
-function openmenu(){
-    document.getElementById("menu").style.display="block"
+// function openmenu(){
+//     document.getElementById("menu").style.display="block"
+// }
+// function closemenu(){
+//     document.getElementById("menu").style.display="none"
+// }
+
+ var clicked = false;
+// function classToggle(){
+//     if(clicked){
+//         closemenu();
+//         clicked=false;
+//     }
+//     else{
+//         openmenu();
+//         clicked= true;
+//     }
+// }
+// toggle button end
+function closemenuv(x){
+    document.getElementById(x).style.display="none"
 }
-function closemenu(){
-    document.getElementById("menu").style.display="none"
+function openmenuv(x){
+    document.getElementById(x).style.display="block"
+    
+        
 }
 
-var clicked = false;
-function classToggle(){
+function togglecontent(x){
     if(clicked){
-        closemenu();
-        clicked=false;
+        closemenuv(x);
+        clicked= false;
     }
     else{
-        openmenu();
+        openmenuv(x);
         clicked= true;
     }
 }
-// toggle button end
